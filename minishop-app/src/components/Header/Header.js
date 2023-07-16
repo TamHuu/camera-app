7e0ffae793efac26933e9ed0b3ef5deb70e9a2f2
HuuTam <christopherle0902@gmail.com>
2023-07-16 15:35:18 +0700

    update Header

--- minishop-app/src/components/Header/Header.js
+++ minishop-app/src/components/Header/Header.js
@@ -1,9 +1,12 @@
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import {
+  AiOutlineFieldTime,
+  AiOutlinePhone,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
 import "./Header.scss";
 function Header() {
   return (
@@ -11,31 +14,53 @@ function Header() {
       <div className="container">
         <Navbar expand="lg" className="bg-body-tertiary">
           <Container fluid>
-            <Navbar.Brand href="#">Electronic Device</Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Brand href="#" className="logo">
+              THIẾT BỊ ĐIỆN TỬ
+            </Navbar.Brand>
+
+            <Navbar.Brand href="#" className="contain_cart">
+              <div class="d-lg-none">
+                <div className="shopping_Cart">
+                  <AiOutlineShoppingCart />
+                </div>
+              </div>
+            </Navbar.Brand>
+
             <Navbar.Collapse id="navbarScroll">
               <Nav className="me-auto my-2 my-lg-0" navbarScroll>
-                <Nav.Link href="#action1">Home</Nav.Link>
-                <NavDropdown title="Products" id="navbarScrollingDropdown">
-                  <NavDropdown.Item href="#action3">Camera</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">Iphone</NavDropdown.Item>
+                <Nav.Link href="#action1">TRANG CHỦ</Nav.Link>
+                <Nav.Link href="#action2">GIỚI THIỆU</Nav.Link>
+                <NavDropdown title="SẢN PHẨM" id="navbarScrollingDropdown">
+                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Another action
+                  </NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item href="#action5">
                     Something else here
                   </NavDropdown.Item>
                 </NavDropdown>
-                <Nav.Link href="#action1">About</Nav.Link>
-                <Nav.Link href="#action2">Contact</Nav.Link>
+                <Nav.Link href="#action3">DỊCH VỤ</Nav.Link>
+                <Nav.Link href="#action4">LIÊN HỆ</Nav.Link>
               </Nav>
-              <Form className="d-flex">
-                <Form.Control
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-              </Form>
+              <div className="contacts">
+                <ul className="contact">
+                  <li>
+                    <span className="icon">
+                      <AiOutlineFieldTime />
+                    </span>
+                    Hỗ trợ 24/7
+                  </li>
+                  <li className="icon">|</li>
+                  <li>
+                    <span className="icon">
+                      <AiOutlinePhone />
+                    </span>
+                    0789.840.902
+                  </li>
+                </ul>
+              </div>
             </Navbar.Collapse>
           </Container>
         </Navbar>
